6dc1fae73514574ade3ffa82d06225565bc81e7f
Elhadj Alpha Amadou Diallo <Dialloalphaamadou947@gmail.com>
2025-03-01 11:31:34 +0000

    pwa: en cours

--- src/App.tsx
+++ src/App.tsx
@@ -139,7 +139,7 @@ function App() {
         <div className="fixed bottom-4 right-4 z-50">
           <button
             onClick={handleInstallClick}
-            className="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-green-500 transition"
+            className="flex items-center gap-2 bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-green-500 transition"
           >
             <DownloadCloud className="h-5 w-5" />
             Installer l'application dans votre telephone
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,67 @@
+// // src/components/Hero.tsx
+// import React from 'react';
+// import { Shield, Zap, CheckCircle } from 'lucide-react';
+
+// interface HeroProps {
+//   setShowDemoForm: (show: boolean) => void;
+// }
+
+// export function Hero({ setShowDemoForm }: HeroProps) {
+//   return (
+//     <div className="container mx-auto px-4 pt-20 pb-12 md:pt-28 md:pb-20 font-sans">
+//       <div className="flex flex-col gap-8 md:grid md:grid-cols-2 md:gap-18 items-center">
+//         {/* Texte */}
+//         <div className="space-y-0 text-center md:text-left">
+//           <h2 className="text-xl md:text-5xl lg:text-6xl text-center mb-4 leading-tight md:mb-10">
+//             Connectez vos boutiques avec <span className="text-green-600 font-extrabold tracking-wide">Xstore</span>
+//           </h2>
+//         </div>
+
+//         {/* Image avec points forts */}
+//         <div className="relative w-full">
+//           <img
+//             src="utilisateur1.jpg"
+//             alt="Propriétaire de boutique au Marché Madina utilisant XStore"
+//             className="rounded-2xl shadow-2xl w-full h-auto object-cover aspect-video"
+//           />
+//           {/* Drapeau de la Guinée */}
+//           <img
+//             src="guinee.jpeg"
+//             alt="Drapeau de la Guinée"
+//             className="absolute top-2 right-2 w-6 h-4 sm:w-8 sm:h-5 md:w-10 md:h-6 rounded-sm shadow-md"
+//           />
+
+//           {/* Points forts superposés à droite */}
+//           <div className="absolute right-2 sm:right-4 md:right-6 top-4 sm:top-6 md:top-8 flex flex-col gap-3 sm:gap-4 md:gap-5">
+//             {/* Point 1: Automatisation */}
+//             <div className="bg-white p-2 sm:p-3 md:p-4 rounded-lg shadow-lg border border-gray-100 flex items-center gap-2 w-40 sm:w-48 md:w-56">
+//               <Shield className="h-4 w-4 sm:h-5 sm:w-5 md:h-6 md:w-6 text-green-600 flex-shrink-0" />
+//               <div> 
+//                 <p className="text-[10px] sm:text-xs md:text-sm text-gray-600">En temps réel</p>
+//               </div>
+//             </div>
+
+//             {/* Point 2: Vitesse */}
+//             <div className="bg-white p-2 sm:p-3 md:p-4 rounded-lg shadow-lg border border-gray-100 flex items-center gap-2 w-40 sm:w-48 md:w-56">
+//               <Zap className="h-4 w-4 sm:h-5 sm:w-5 md:h-6 md:w-6 text-green-600 flex-shrink-0" />
+//               <div> 
+//                 <p className="text-[10px] sm:text-xs md:text-sm text-gray-600">Traitement rapide</p>
+//               </div>
+//             </div>
+
+//             {/* Point 3: Praticité */}
+//             <div className="bg-white p-2 sm:p-3 md:p-4 rounded-lg shadow-lg border border-gray-100 flex items-center gap-2 w-40 sm:w-48 md:w-56">
+//               <CheckCircle className="h-4 w-4 sm:h-5 sm:w-5 md:h-6 md:w-6 text-green-600 flex-shrink-0" />
+//               <div> 
+//                 <p className="text-[10px] sm:text-xs md:text-sm text-gray-600">Facile à utiliser</p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 // src/components/Hero.tsx
 import React from 'react';
 import { Shield, Star } from 'lucide-react';
@@ -13,7 +77,7 @@ export function Hero({ setShowDemoForm }: HeroProps) {
         {/* Texte */}
         <div className="space-y-0 text-center md:text-left"> {/* Augmentation du space-y */}
           <h2 className="text-xl md:text-5xl lg:text-6xl text-center mb-4 leading-tight md:mb-10">  
-            Connectez vos boutiques avec <span className="text-green-600 font-extrabold tracking-wide">XStore</span>
+            Connectez vos boutiques avec <span className="text-green-600 font-extrabold tracking-wide">Xstore</span>
           </h2>
         </div>
 
